4a8ea0f3762142738c4701725900de01620c30f4
oyunmintrio95 <oyunmintrio95@gmail.com>
2023-10-08 22:07:20 -0700

    updated About card

--- fansite-project/sql/fansite-production-schema.sql
+++ fansite-project/sql/fansite-production-schema.sql
@@ -6,7 +6,7 @@ create table about (
 	about_id int primary key auto_increment,
     first_name varchar(50) not null,
     last_name varchar(50) not null,
-    about_description varchar(200) not null,
+    about_description text not null,
     git varchar(50) not null
 );
 
@@ -18,22 +18,7 @@ create table forum (
     post_date date null
 );
 
-delimiter //
-create procedure set_known_good_state()
-begin
-	delete from about;
-	delete from forum;
-	alter table about auto_increment=1;
-	alter table forum auto_increment=1;
-	
-    
-    insert into about (first_name, last_name, about_description, git) values
-		('Tony', 
-        'Lee', 
-        'I am a huge Monster Hunter fan, with a B.S. in Mathematics and Computer Science. I enjoy playing tennis, video games, and reading. My favorite monster from the Monster Hunter Franchise is the Rathalos, and my most recent favorite weapon is the Bow, previously it was the Switch Axe. ','github.com/tal154');
-        
-	insert into forum (forum_name, title, post_content, post_date) values
-		('user 1', 'post 1', 'I am a monster hunter fan', '2020-11-11'),
-        ('user 2', 'post 2', 'I am not a monster hunter fan', '2020-12-12');
-end//
-delimiter ;
+insert into about (first_name, last_name, about_description, git) values
+		('Tony', 'Lee', 'I am a huge Monster Hunter fan, with a B.S. in Mathematics and Computer Science. I enjoy playing tennis, video games, and reading. My favorite monster from the Monster Hunter Franchise is the Rathalos, and my most recent favorite weapon is the Bow, previously it was the Switch Axe. ','github.com/tal154'),
+		('Hayoung', 'Cheon', 'Passionate and fast adapting backend developer with some experiences of designing, building, and enhancing JAVA spring boot web application projects. I have a B.S. in Elementary Education and Computer Education. I enjoy camping, hiking and solving puzzles.','github.com/oyunmintrio95'),
+		('Autumn', 'Sprout', 'Passionate and fast adapting backend developer with some experiences of designing, building, and enhancing JAVA spring boot web application projects. I have a B.S. in Elementary Education and Computer Education. I enjoy camping, hiking and solving puzzles.','github.com/jotasprout');
