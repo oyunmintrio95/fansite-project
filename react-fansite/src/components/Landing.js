c5dfddbe449d2ce6a0dacb412d788649bf6841a3
oyunmintrio95 <oyunmintrio95@gmail.com>
2023-10-09 00:00:00 -0700

    final update hayoung

--- react-fansite/src/components/Landing.js
+++ react-fansite/src/components/Landing.js
@@ -1,3 +1,5 @@
+import landingVd from "../image/LandingBackground.mp4";
+
 function Landing() {
     return (
         <div className="container-fluid">
@@ -5,6 +7,9 @@ function Landing() {
             <div className="my-4" >
                 <img className="img-fluid border border-secondary rounded-5"
                     src="https://portforward.com/monster-hunter-world/default.webp" alt="agents in a tunnel" />
+            </div>
+            <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4'>
+                
             </div>
         </div>
     );
