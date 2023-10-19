ddc9a3fc6e5aa8c56b5e9d0cfc8ac7b231fdcf00
Jay Sprout <jotasprout@gmail.com>
2023-10-19 15:57:30 +0200

    changed alt-text for landing image

--- react-fansite/src/components/Landing.js
+++ react-fansite/src/components/Landing.js
@@ -6,7 +6,7 @@ function Landing() {
             <h1 className="display-5">Monster Hunter World Fansite</h1>
             <div className="my-4" >
                 <img className="img-fluid border border-secondary rounded-5"
-                    src="https://portforward.com/monster-hunter-world/default.webp" alt="agents in a tunnel" />
+                    src="https://portforward.com/monster-hunter-world/default.webp" alt="pretty magical land" />
             </div>
             <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4'>
                 
